f18ef5bf18732e98157cf1af5ee93c594331430c
RubertiN <nikiruberti@gmail.com>
2022-06-15 17:03:10 +0200

    giulio

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function SendMail() {
-    var params = {
+emailjs.sendForm(params) 
+    var tempParams ={
         from_name : document.getElementById("full-name").value,
-        email_id : document.geteElementById("e-mail").value,
-        message : document.getElementById("messaggio").value
-    }
-    emailjs.send("service_texfjf6", "template_s97obig", params).then(function (res) {
-        alert("Success! " + res.status);
+        email_id : document.getElementById("e-mail").value,
+        message : document.getElementById("messaggio").value,
+    };
+
+    emailjs.send('service_texfjf6', 'template_s97obig', tempParams)
+    .then(function(res){
+        console.log("success", res.status);
     })
-}
